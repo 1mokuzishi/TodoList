c06c29ae5f267026611c4dea642026dce5dae506
Trolife Yu <yushuai1996@live.com>
2018-12-27 18:41:38 +0800

    create gh-pages
    create to show React TodoList page

---
+++ precache-manifest.b8b7b9f51dfd4e5c864e07268bc7da85.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b1104f616cd8d5a74023",
+    "url": "./static/js/main.b1104f61.chunk.js"
+  },
+  {
+    "revision": "701e80665e0aa5267d43",
+    "url": "./static/js/1.701e8066.chunk.js"
+  },
+  {
+    "revision": "b1104f616cd8d5a74023",
+    "url": "./static/css/main.88fdd654.chunk.css"
+  },
+  {
+    "revision": "f83a3d4a579625c0994eb2fe2235dccd",
+    "url": "./index.html"
+  }
+];
